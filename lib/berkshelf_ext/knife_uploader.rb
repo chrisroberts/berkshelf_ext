8cc5bc55f6e6593bf9620100e7cd96c61105e576
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-10 20:31:44 -0700

    Add support for using knife's uploader instead of ridley

---
+++ lib/berkshelf_ext/knife_uploader.rb
@@ -0,0 +1,62 @@
+require 'chef'
+require 'chef/cookbook_uploader'
+
+module Ridley
+  class << self
+    def new(options)
+      RidleyMocker.new(options)
+    end
+  end
+end
+
+class RidleyMocker
+  def initialize(options)
+  end
+
+  def method_missing(sym, *args)
+    if(@ckbk)
+      @ckbk.send(sym, *args)
+    else
+      super
+    end
+  end
+  
+  def cookbook
+    self
+  end
+
+  def alive?
+    false
+  end
+
+  def path
+    @path
+  end
+  
+  def upload(path, upload_options)
+    chef_config!
+    @path = path
+    loader = Chef::Cookbook::CookbookVersionLoader.new(path)
+    loader.load_cookbooks
+    cv = loader.cookbook_version
+    cv.send(:generate_manifest)
+    cv.manifest['name'] = "#{cv.name}-#{cv.version}" #.sub!(%r{-[^-]+$}, '')
+    cv.manifest['cookbook_name'] = cv.name
+    cv.freeze_version if upload_options.delete(:freeze)
+    Chef::CookbookUploader.new([cv], cookbook.path, upload_options).upload_cookbooks
+    @ckbk = cv
+  end
+
+  def chef_config!
+    cwd = Dir.pwd.split('/')
+    until(cwd.empty?)
+      knife_conf = File.join(cwd.join('/'), '.chef/knife.rb')
+      if(File.exists?(knife_conf))
+        Chef::Config.from_file(knife_conf)
+        return
+      end
+    end
+    raise 'Failed to locate knife.rb file to configure chef!'
+  end
+      
+end

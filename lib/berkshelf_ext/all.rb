01d55acdd0c161b8b4a31da1f15cb1e596424da5
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-11 08:16:33 -0700

    Provide env variable that will enable everything

--- lib/berkshelf_ext/all.rb
+++ lib/berkshelf_ext/all.rb
@@ -1,17 +1,23 @@
 require 'berkshelf'
 
-Dir.glob(File.join(File.dirname(__FILE__), '*.rb')).each do |ext_file|
-  if(ENV['BERKSHELF_EXT_EXCEPT'])
-    next if ENV['BERKSHELF_EXT_EXCEPT'].split(',').include?(File.basename(ext_file).sub('.rb', ''))
+if(ENV['BERKSHELF_EXT_EVERYTHING'].to_s.downcase == 'true')
+  Dir.glob(File.join(File.dirname(__FILE__), '**', '*.rb')).each do |ext_file|
+    require ext_file
   end
-  if(ENV['BERKSHELF_EXT_ONLY'])
-    next unless ENV['BERKSHELF_EXT_ONLY'].split(',').include?(File.basename(ext_file).sub('.rb', ''))
+else
+  Dir.glob(File.join(File.dirname(__FILE__), '*.rb')).each do |ext_file|
+    if(ENV['BERKSHELF_EXT_EXCEPT'])
+      next if ENV['BERKSHELF_EXT_EXCEPT'].split(',').include?(File.basename(ext_file).sub('.rb', ''))
+    end
+    if(ENV['BERKSHELF_EXT_ONLY'])
+      next unless ENV['BERKSHELF_EXT_ONLY'].split(',').include?(File.basename(ext_file).sub('.rb', ''))
+    end
+    require ext_file
   end
-  require ext_file
-end
 
-if(ENV['BERKSHELF_EXT_ADDONS'])
-  ENV['BERKSHELF_EXT_ADDONS'].split(',').each do |addon|
-    require "berkshelf_ext/addons/#{addon.strip}"
+  if(ENV['BERKSHELF_EXT_ADDONS'])
+    ENV['BERKSHELF_EXT_ADDONS'].split(',').each do |addon|
+      require "berkshelf_ext/addons/#{addon.strip}"
+    end
   end
 end
